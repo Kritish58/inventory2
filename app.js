11b3ed63bf6198e961a45eb000f1ffe277c68a7e
Rishi58 <rishi58rishi@gmail.com>
2020-08-06 21:00:16 +0545

    attermpt fix

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cors = require('cors');
+const path = require('path');
 const fileUpload = require('express-fileupload');
 const express = require('express');
 const app = express();
@@ -14,5 +15,8 @@ app.use(express.json());
 
 app.use('/api/categories', categoreiesAPI);
 app.use('/api/items', itemsAPI);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
 
 module.exports = app;
